137fc65d9aa82f5a8e91f2ce4fab937ad1c8b61b
Jieun Rim <rim.jieun@gmail.com>
2017-02-25 07:41:33 -0500

    changes made

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,55 +9,71 @@ function Character(name, healthPoints, basePower, counterAttack, attackPower) {
 }
 
 
+//------------------------------------------START GAME--------------------------------------------------
 function startGame() {		//study .wrapInner()
 
-	//more efficient way to update characters?
-	//HAVE TO USE JQUERY TO CREATE DIV FROM SCRATCH AND CREATE CHILDREN IN DIV T_T
+	$("#characterList").appendTo("#heading");
+	$("input").remove();
+
+//----------------------------------Creating First Character--------------------------------------------
 	var character1 = new Character("Obi-Wan Kenobi", 120, 5, 10, 0);
 	$("#character1 > .name").html(character1.name);
-	$("#character1 > .name").after('<img src="assets/images/obi-wan-kenobi.jpg" alt="Obi-Wan Kenobi">');
+	$("#character1 > .image").html('<img src="assets/images/obi-wan-kenobi.jpg" alt="Obi-Wan Kenobi">');
 	$("#character1 > .healthPoints").html(character1.healthPoints);
 	$("#character1").attr("data-health-points", character1.healthPoints);
 	$("#character1").attr("data-base-power", character1.basePower);
 	$("#character1").attr("data-counter-attack", character1.counterAttack);
 	$("#character1").attr("data-attack-power", character1.attackPower);
 
+//----------------------------------Creating Second Character--------------------------------------------
 	var character2 = new Character("Luke Skywalker", 100, 5, 10, 0);
 	$("#character2 > .name").html(character2.name);
-	$("#character2 > .name").after('<img src="assets/images/luke-skywalker.jpg" alt="Luke Skywalker">');
+	$("#character2 > .image").html('<img src="assets/images/luke-skywalker.jpg" alt="Luke Skywalker">');
 	$("#character2 > .healthPoints").html(character2.healthPoints);
 	$("#character2").attr("data-health-points", character2.healthPoints);
 	$("#character2").attr("data-base-power", character2.basePower);
 	$("#character2").attr("data-counter-attack", character2.counterAttack);
 	$("#character2").attr("data-attack-power", character2.attackPower);
 
+//----------------------------------Creating Third Character--------------------------------------------
 	var character3 = new Character("Darth Sidious", 150, 5, 10, 0);
 	$("#character3 > .name").html(character3.name);
-	$("#character3 > .name").after('<img src="assets/images/darth-sidious.png" alt="Darth Sidious">');
+	$("#character3 > .image").html('<img src="assets/images/darth-sidious.png" alt="Darth Sidious">');
 	$("#character3 > .healthPoints").html(character3.healthPoints);
 	$("#character3").attr("data-health-points", character3.healthPoints);
 	$("#character3").attr("data-base-power", character3.basePower);
 	$("#character3").attr("data-counter-attack", character3.counterAttack);
 	$("#character3").attr("data-attack-power", character3.attackPower);
 
+//----------------------------------Creating Fourth Character--------------------------------------------
 	var character4 = new Character("Darth Maul", 180, 5, 25, 0);
 	$("#character4 > .name").html(character4.name);
-	$("#character4 > .name").after('<img src="assets/images/darth-maul.jpeg" alt="Darth Maul">');
+	$("#character4 > .image").html('<img src="assets/images/darth-maul.jpeg" alt="Darth Maul">');
 	$("#character4 > .healthPoints").html(character4.healthPoints);
 	$("#character4").attr("data-health-points", character4.healthPoints);
 	$("#character4").attr("data-base-power", character4.basePower);
 	$("#character4").attr("data-counter-attack", character4.counterAttack);
 	$("#character4").attr("data-attack-power", character4.attackPower);
 
+	var characters = [character1, character2, character3, character4];
+
+	var newChars = characters.map(doJQueryStuff)	// use map to create characters!
+
+//----------------------------------Creating Characters using MAP----------------------------------------
+	function doJQueryStuff(char, index) {
+	 	$("#character" + index + " > .name").html(char.name);
+	}
 
 	var locked = false;
 
+//----------------------------------------SELECT CHARACTERS----------------------------------------------
 	function chooseCharacters() {
 		$(".characters").on("click", function() {
 			if (locked === false) {
 				$(this).appendTo("#yourCharacter");
 				if ($("#yourCharacter > div").hasClass("characters")) {
-					$("#characterList").appendTo("#enemiesAvailable");
+					var enemies = $("#characterList").toArray();
+					$(enemies).appendTo("#enemiesAvailable");
 				}
 				locked = true;
 			}
@@ -69,6 +85,8 @@ function startGame() {		//study .wrapInner()
 
 	chooseCharacters();
 
+
+//--------------------------------------------ATTACK BUTTON------------------------------------------------
 	$("#attackButton").on("click", function() {
 		if ($("#defender > div").hasClass("characters")) {
 			// console.log("attack");
@@ -104,25 +122,32 @@ function startGame() {		//study .wrapInner()
 					$("#yourCharacter > div > .healthPoints").html(yourCharHealth);
 					$("#defender > div > .healthPoints").html(defenderHealth);
 
-					$("#gameProgress1").html("You attacked " + defenderName + " for " + yourCharAttack + " damage.");
-					$("#gameProgress2").html(defenderName + " attacked you back for " + defenderCounter + " damage.");
+					$("#line1").html("You attacked " + defenderName + " for " + yourCharAttack + " damage.");
+					$("#line2").html(defenderName + " attacked you back for " + defenderCounter + " damage.");
 
 				}
 
-				else if (yourCharHealth < 0) {
-					$("#gameProgress1").html("You have been defeated...GAME OVER!");
+				if (yourCharHealth < 0) {
+					$("#line1").html("You have been defeated...GAME OVER!");
 					
 					var button = $('<input type="button" value="Restart" />');
 
-					$("#gameProgress2").html(button).on("click", function() {
-						startGame(); //ERROR: APPENDS CHARACTERS INSTEAD OF RESETTING GAME
-					});
+					button = $("#line2").html(button);
+
+					button.attr("id", "button");
+
+					$("#button").on("click", function() {
+	$("#character1, #character2, #character3, #character4").appendTo($("#characterList"));
+	startGame();
+})
 				}
+
+
 
 				else if (defenderHealth < 0) {
 					defenderDiv.remove();
-					$("#gameProgress1").html("You have defeated " + defenderName + ". You can choose to fight another enemy.");
-					$("#gameProgress2").empty();
+					$("#line1").html("You have defeated " + defenderName + ". You can choose to fight another enemy.");
+					$("#line2").empty();
 					chooseCharacters();
 				}
 
@@ -131,15 +156,20 @@ function startGame() {		//study .wrapInner()
 			attack();
 		}
 		else {
-			$("#gameProgress1").html("No enemy here."); //appearing in wrong place
+			$("#line1").html("No enemy here.");
 		}
 
 	})
 
 
+
 }
 
+//------------------------------------------PLAY GAME-------------------------------------------------
+
 startGame();
+
+
 
 
 	
